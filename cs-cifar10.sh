dbec343d0ff40443d146fca291ed57588c6885bd
Yu Gai <yg1246@nyu.edu>
2018-08-09 02:33:00 +0000

    commit

--- cs-cifar10.sh
+++ cs-cifar10.sh
@@ -6,7 +6,7 @@ for bs in 32 64; do
             for t in $(seq 1 $((n - 1))); do
                 w_pos=$(python3 -c "print(round(2 - 2 * $t / $n, 1))")
                 w_neg=$(python3 -c "print(round(2 * $t / $n, 1))")
-                python3 train.py --bs $bs --bsi 4096 --ds cifar10 --gpu $gpu --log-every 1 --lr $lr --model resnet --ni 1000 --ptt 4 1 1 --srgt $srgt --tb --update-every 0 --w-pos $w_pos --w-neg $w_neg
+                python3 train.py --bs $bs --bsi 4096 --ds cifar10 --gpu $gpu --log-every 1 --lr $lr --model resnet --ni 1000 --ptt 4 1 1 --srgt $srgt --tb --update-every 0 --w-pos $w_pos --w-neg $w_neg &
                 gpu=$((gpu + 1))
                 if ! (($gpu % 4)); then
                     gpu=0
