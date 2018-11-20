4131711e8cb350fa5a06946068e821078767dc8c
GaiYu0 <yg1246@nyu.edu>
2018-11-20 09:54:26 +0800

    commit

--- new-binary.sh
+++ new-binary.sh
@@ -3,7 +3,7 @@ i=0
 for bs in 64 128 256 512; do
     for lr in 0.1 0.01 0.001 0.0001; do
         for wd in 0.1 0.01 0.001 0.0001; do
-            python3 binary.py --bsi 262144 --bs-pos $bs --bs-neg $bs --ds $1 --gpu $(($i % 4)) --id $bs-$lr-$wd --log-every 100 --lr $lr --model mlp --ni 10000 --opt adam --ptt 6 2 2 --tb --w 0 --wd $wd &
+            python3 binary.py --bsi 262144 --bs-pos $bs --bs-neg $bs --ds $1 --gpu $(($i % 4)) --id $bs-$lr-$wd --log-every 100 --lr $lr --model linear --ni 10000 --opt adam --ptt 6 2 2 --tb --w 0 --wd $wd &
             i=$(($i + 1))
             if (($i % $2 == 0)); then
                 wait
