0f7810dff4c8fdc4dc80ae9521834ad483e8be8a
Ubuntu <ubuntu@ip-172-31-93-232.ec2.internal>
2019-01-15 01:34:18 +0000

    sync

--- optimal.sh
+++ optimal.sh
@@ -1,8 +1,15 @@
+# bash optimal.sh INPUT_PATH METRIC NUMBER_OF_PROCESSES OUTPUT_FILE
+input=$1
+metric=$2
+n_procs=$3
+output=$4
 i=0
-for x in $1/*; do
-    echo "$x: $(python3 -W ignore optimal.py $x f1)" >> $2 &
+for x in $input/*; do
+    echo "$x: $(python3 -W ignore optimal.py $x $metric)" >> x$output$i &
     i=$(($i + 1))
-    if (($i % $3 == 0)); then
+    if (($i % $n_procs == 0)); then
         wait
     fi
 done
+cat x$output* >> $output
+rm x$output*
