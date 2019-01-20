b35625d6f4b952b2b423e9628e69602d28b0dd8c
Yu Gai <yg1246@nyu.edu>
2019-01-20 14:33:46 +0800

    sync

--- select-iteration.sh
+++ select-iteration.sh
@@ -1,11 +1,11 @@
-# bash optimal.sh INPUT_PATH METRIC NUMBER_OF_PROCESSES OUTPUT_FILE
+# bash select-iteration.sh INPUT_PATH METRIC NUMBER_OF_PROCESSES OUTPUT_FILE
 input=$1
 metric=$2
 n_procs=$3
 output=$4
 i=0
 for x in $input/*; do
-    echo "$x: $(python3 -W ignore optimal.py $x $metric)" > $output$i- &
+    echo "$x: $(python3 -W ignore select-iteration.py $x $metric)" > $output$i- &
     i=$(($i + 1))
     if (($i % $n_procs == 0)); then
         wait
